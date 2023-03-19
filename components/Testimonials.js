efffd1a057bf971dd52b94be24f1e1d89d44758d
rtungol <rarrtungol@gmail.com>
2023-03-19 01:31:10 -0400

    change layout using tailwind

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,33 +1,86 @@
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useState } from "react";
+
+const recommendations = [
+  {
+    name: "John Smith",
+    title: "CEO",
+    company: "ABC Inc.",
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    name: "Jane Doe",
+    title: "CTO",
+    company: "XYZ Corp.",
+    quote:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+  },
+  {
+    name: "David Lee",
+    title: "COO",
+    company: "123 Co.",
+    quote:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.",
+  },
+];
+
+function SimpleSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [countdown, setCountdown] = useState(7);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((countdown) => countdown - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [currentSlide]);
+
+  var settings = {
+    dots: true,
+    fade: true ,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (index) => {
+      setCurrentSlide(index);
+      setCountdown(7);
+    },
+  };
+  return (
+    <div className="mx-auto max-w-2xl lg:max-w-4xl lg:py-20">
+      <Slider {...settings}>
+        {recommendations.map((recommendation) => (
+          <figure className="mt-10" key={recommendation.name}>
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>{recommendation.quote}</p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div className="font-semibold text-gray-900">
+                  {recommendation.name}
+                </div>
+
+                <div className="text-gray-600">{`${recommendation.title} of ${recommendation.company}`}</div>
+              </div>
+            </figcaption>
+          </figure>
+        ))}
+      </Slider>
+      <div className="mt-4 text-center p-5">Next slide in {countdown}s</div>
+    </div>
+  );
+}
+
 export default function Testimonials() {
   return (
-    <section className="relative isolate overflow-hidden bg-white py-24 px-6 sm:py-32 lg:px-8">
-      <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-      <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
-        <figure className="mt-10">
-          <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-            <p>
-              “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-              molestiae. Numquam corrupti in laborum sed rerum et corporis.”
-            </p>
-          </blockquote>
-          <figcaption className="mt-10">
-            <img
-              className="mx-auto h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-              <div className="font-semibold text-gray-900">Judith Black</div>
-              <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                <circle cx={1} cy={1} r={1} />
-              </svg>
-              <div className="text-gray-600">CEO of Workcation</div>
-            </div>
-          </figcaption>
-        </figure>
-      </div>
+    <section className="flex justify-center">
+      <SimpleSlider />
     </section>
-  )
+  );
 }
